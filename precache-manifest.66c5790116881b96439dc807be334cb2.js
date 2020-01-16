9e4398ee0cfc4c1178c2924492f41faca0fc95cd
Reza Erami <reer217@gmail.com>
2020-01-16 19:50:23 +0330

    initiated webpage

---
+++ precache-manifest.66c5790116881b96439dc807be334cb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa93168232ff551c08be80b2166220b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41b15bf516a572d87855",
+    "url": "/static/css/main.3455b03b.chunk.css"
+  },
+  {
+    "revision": "bb08a153e8ee604d3563",
+    "url": "/static/js/2.7edb4c23.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7edb4c23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41b15bf516a572d87855",
+    "url": "/static/js/main.7dced099.chunk.js"
+  },
+  {
+    "revision": "17624ccbd617d978d5ce",
+    "url": "/static/js/runtime-main.0750644b.js"
+  },
+  {
+    "revision": "d73749f8983c6da833aa5418b901a5d2",
+    "url": "/static/media/logo.d73749f8.png"
+  }
+]);
